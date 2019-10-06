73d1014999a8e42d1b371cb90af152d70d2606c6
Niel Ponciano <nielgp93@gmail.com>
2019-10-06 09:44:51 +0800

    Added next_permutation but for string only for now

---
+++ src/Algorithms/Next_permutation.hpp
@@ -0,0 +1,34 @@
+#ifndef SRC_ALGORITHMS_NEXT_PERMUTATION_HPP
+#define SRC_ALGORITHMS_NEXT_PERMUTATION_HPP
+
+#include <string>
+
+namespace mystl
+{
+
+bool next_permutation(std::string& stringToPermutateSorted)
+{
+    for (int ctr = stringToPermutateSorted.size() - 1; ctr > 0; --ctr)
+    {
+        // Early continue is nice to avoid such nested {{{}}}. But to be clear with the purpose of
+        // this block that finds first occurrence of greater item[ctr] than item[ctr-1] thus is
+        // preferred for increased readability of purpose
+        if (stringToPermutateSorted[ctr] > stringToPermutateSorted[ctr-1])
+        {
+            for (int ctr2 = stringToPermutateSorted.size() - 1; ctr2 >= ctr; --ctr2)
+            {
+                if (stringToPermutateSorted[ctr2] > stringToPermutateSorted[ctr-1])
+                {
+                    std::swap(stringToPermutateSorted[ctr2], stringToPermutateSorted[ctr-1]);
+                    std::sort((stringToPermutateSorted.begin() + ctr), stringToPermutateSorted.end());
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+} // namespace mystl
+
+#endif // SRC_ALGORITHMS_NEXT_PERMUTATION_HPP
